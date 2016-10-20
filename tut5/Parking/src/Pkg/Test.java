fb47f37bf51ccbcbca73c2d589d3281cf5f14d0d
gimy boya <b.afatah@yahoo.com>
2016-10-21 02:04:12 +0800

    update the last exercise

--- tut5/Parking/src/Pkg/Test.java
+++ tut5/Parking/src/Pkg/Test.java
@@ -7,34 +7,42 @@ public class Test {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        Scanner input2 = new Scanner(System.in);
 
-        Parking[] parkingToday = new Parking[300];
+        Parking[] parkingToday = new Parking[300]; // we declare and create our array but notice that we didn't create objects inside it yet
 
         System.out.println("How many vehicles we have today ? (enter an integer no more than 300): ");
 
-        int numberOfVehicules = 0;
+        int numberOfVehicles = 0;
 
         int type = 0;
         int hours = 0;
 
         double todayFees = 0;
 
+        while (!input.hasNextInt()){
+            System.out.println("Please enter an integer and nothing else");
+            input.next();
+
+        }
+
+        numberOfVehicles = input.nextInt();
 
 
-        numberOfVehicules = input.nextInt();
+        for (int i = 0; i < numberOfVehicles ; i++) { // this is a loop to create the vehicles inside the array
 
 
-        for (int i = 0; i < numberOfVehicules ; i++) {
+            do{
+                System.out.println("What is the type of vehicle? : (1-Car    2-Motorbike): ");
 
-            System.out.println("What is the type of vehicle? : (1-Car    2-Motorbike): ");
-            while (!input.hasNextInt() ){
-                System.out.println("Please enter an integer and nothing else");
-                input.next();
+                while (!input.hasNextInt()){
+                    System.out.println("Please enter the numbers 1 or 2");
+                    input.next();
 
-            }
+                }
+                type = input.nextInt();
+            }while (type != 1 && type != 2);
 
-            type = input.nextInt();
+
 
             System.out.println("How many hours is it parked?: (enter an integer) ");
             while (!input.hasNextInt() ){
@@ -47,6 +55,14 @@ public class Test {
 
             parkingToday[i] = new Parking(type, hours);
         }
+
+        for (int i = 0; i < numberOfVehicles; i++) { // this is a loop to calculate the fees of all the vehicles
+
+            todayFees = todayFees + parkingToday[i].getFees();
+        }
+
+        System.out.println("The total fees for today are: " + todayFees);
+
 
 
 
