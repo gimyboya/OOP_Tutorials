7a3d84fb6671cfd62750d04d8d0d5688782de777
gimy boya <b.afatah@yahoo.com>
2016-11-09 23:50:03 +0800

    assigment solution 6

--- tut7/Assigment/src/Pkg/Test.java
+++ tut7/Assigment/src/Pkg/Test.java
@@ -1,16 +1,111 @@
 package Pkg;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
 
-        Account acount1 = new Account(1122,20000,4.5);
+        Scanner input = new Scanner(System.in);
 
-        acount1.withdraw(2500);
-        acount1.deposit(3000);
 
-        System.out.println(acount1.getMonthlyInterest());
-        System.out.println("this account was created om : " + acount1.getDateCreated());
+
+
+        ArrayList<Account> Accounts = new ArrayList<Account>();
+
+        for (int i = 0; i < 10; i++) {
+            Accounts.add(i,new Account(i,100,4.5));
+        }
+
+
+
+        do{
+            int id = -1;
+            do{
+                System.out.println("Please enter your account ID [0 - 9] : ");
+
+                while (!input.hasNextInt()){
+                    System.out.println("Please enter an Integer");
+                    input.next();
+                }
+
+                id = input.nextInt();
+
+            }while (id > 9 || id < 0);
+
+            boolean exit = false;
+
+            do{
+                int choice = -1;
+
+                System.out.println("Main menu");
+                System.out.println("1:check balance");
+                System.out.println("2:withdraw");
+                System.out.println("3:deposit");
+                System.out.println("4:exit");
+
+
+                do{
+                    System.out.println("Enter a choice [1 - 4] : ");
+
+                    while (!input.hasNextInt()){
+                        System.out.println("Please enter an Integer");
+                        input.next();
+                    }
+
+                    choice = input.nextInt();
+
+                }while (choice > 4 || choice < 1);
+
+                if(choice == 1){
+                    System.out.println("Your Balance is : " + Accounts.get(id).getBalance());
+                }else if(choice == 2){
+                    double amount = 0;
+                    System.out.println("What is the amount you want to withdraw? : ");
+
+                    while (!input.hasNextDouble()){
+                        System.out.println("Please enter a correct number");
+                        input.next();
+                    }
+
+                    amount = input.nextDouble();
+
+                    if(amount > Accounts.get(id).getBalance()){
+                        System.out.println("Sorry your account only has " + Accounts.get(id).getBalance());
+                    }else{
+                        Accounts.get(id).withdraw(amount);
+                        System.out.println("Your account is now " + Accounts.get(id).getBalance());
+                    }
+
+                }else if(choice == 3){
+
+                    System.out.println("What is the amount you want to deposit? : ");
+
+                    double amount = 0;
+
+                    while (!input.hasNextDouble()){
+                        System.out.println("Please enter a correct number");
+                        input.next();
+                    }
+
+                    amount = input.nextDouble();
+
+                    Accounts.get(id).deposit(amount);
+
+                    System.out.println("Your Balance is now : " + Accounts.get(id).getBalance());
+
+                }else if(choice == 4){
+                    exit = true;
+                }
+
+            }while (!exit);
+
+
+
+        }while(true);
+
+
 
     }
 }
